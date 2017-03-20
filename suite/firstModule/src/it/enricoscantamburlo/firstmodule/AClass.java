f9933b6331297dbe98eeaa835997d9fdd796cce7
EnricoScantamburlo <scanti.rulla@gmail.com>
2017-03-20 01:39:15 +0100

    Added ant build file to launch findbugs

---
+++ suite/firstModule/src/it/enricoscantamburlo/firstmodule/AClass.java
@@ -0,0 +1,13 @@
+package it.enricoscantamburlo.firstmodule;
+
+/**
+ *
+ * @author Enrico Scantamburlo <scantamburlo at streamsim.com>
+ */
+public class AClass {
+
+    public void doSomething() {
+        Object a = null;
+        a.toString();
+    }
+}
